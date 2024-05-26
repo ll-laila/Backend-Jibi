d7649b4e747c36092aeaf93af7182281083e5f0a
Enami-FatimaZahrae <120416127+Enami-FatimaZahrae@users.noreply.github.com>
2024-05-26 16:25:20 +0000

    Delete src/main/java/com/projet/demo/repository directory

--- src/main/java/com/projet/demo/repository/CustomerOrderRepo.java
+++
@@ -1,9 +0,0 @@
-package com.projet.demo.repository;
-
-import com.projet.demo.model.CustomerOrder;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CustomerOrderRepo extends JpaRepository<CustomerOrder,Long> {
-}
