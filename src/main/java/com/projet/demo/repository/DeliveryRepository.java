3f085bbf1f56ed2b9654248861d1b1fd469a1c4e
Enami-FatimaZahrae <120416127+Enami-FatimaZahrae@users.noreply.github.com>
2024-05-26 17:24:14 +0000

    Delete src/main/java/com/projet/demo/repository directory

--- src/main/java/com/projet/demo/repository/DeliveryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.projet.demo.repository;
-
-import com.projet.demo.entity.Delivery;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DeliveryRepository extends JpaRepository<Delivery,Long> {
-}
--- src/main/java/com/projet/demo/repository/PaymentAccountRepository.java
+++
@@ -1,9 +0,0 @@
-package com.projet.demo.repository;
-
-import com.projet.demo.entity.PaymentAccount;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PaymentAccountRepository extends JpaRepository<PaymentAccount,Long> {
-}
